252ba249b05af0b24bbf7f08fa12644db0f3e65c
abdulkaeum <abdul.kaeum@hotmail.com>
2021-11-24 12:28:05 +0000

    Test the stock update with fake API data

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\Clients\Client;
 use App\Clients\ClientException;
+use Facades\App\Clients\ClientFactory;
+use App\Clients\StockStatus;
 use App\Models\Retailer;
 use App\Models\Stock;
 use Database\Seeders\RetailerWithProductSeeder;
@@ -27,5 +30,30 @@ class StockTest extends TestCase
 
         // track the stock
         Stock::first()->track();
+    }
+
+    /** @test */
+    public function it_updates_local_stock_status_after_being_tracked()
+    {
+        $this->seed(RetailerWithProductSeeder::class);
+
+        // ClientFactory to determine the appropriate Client
+        // CheckAvailability()
+        // return ['available' => true, 'price' => 9900]
+        ClientFactory::shouldReceive('make')->andReturn(new FakeClient);
+
+        $stock = tap(Stock::first()->track());
+
+        $this->assertTrue($stock->in_stock);
+        $this->assertEquals(9900, $stock->price);
+
+    }
+}
+
+class FakeClient implements Client
+{
+    public function checkAvailability(Stock $stock): StockStatus
+    {
+        return new StockStatus($available = true, $price = 9900);
     }
 }
